14d43ab51033be1123e83162e4bd2a6f4c074559
Martin Mwangi <mwangimartin1904@gmail.com>
2022-04-24 10:16:58 +0300

    Updates

--- routes/departments.route.js
+++ routes/departments.route.js
@@ -5,7 +5,7 @@ const auth = require("../middlewares/auth");
 const schemaValidator = require("../middlewares/schemaValidator");
 const { createDepartmentSchema } = require("../shema-validators/department.validators");
 
-router.get("/", auth, getDepartmentsController);
+router.get("/", getDepartmentsController);
 router.post("/", auth,  schemaValidator(createDepartmentSchema, "body"), createADepartmentController);
 router.delete("/:id", auth, deleteDepartmentController);
 
