ea7d87df9af2c803f656cc5497046d9183eca83c
Martin Mwangi <mwangimartin1904@gmail.com>
2022-07-19 09:58:11 +0300

    Updates

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -2,7 +2,8 @@ module.exports = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch((error) => {
         console.log(error);
         res.status(500).json({
-            message: "Internal Server Error",
+            message: "An error occurred",
+            error: "Internal Server Error",
             error: error.message,
         });
     });
